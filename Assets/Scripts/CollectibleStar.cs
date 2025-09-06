438e6781b1b2b0c2dbc9b4d9140a2cdce2af8c0f
Rafael Jorge <rafaelcamillojorge@gmail.com>
2025-09-06 18:32:29 -0300

    Add Simple Gems and Items Ultimate Animated Customizable Pack
    - Created a new scene for gems with associated metadata.
    - Added scripts for gem animations including rotation, floating, and scaling.
    - Implemented collectible star functionality with score tracking and UI updates.
    - Introduced score service to manage game scores and high scores.
    - Added prefab for a new collectible item (Chao) with necessary components.
    - Updated metadata for all new assets and scripts.

---
+++ Assets/Scripts/CollectibleStar.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleStar : MonoBehaviour
+{
+    static readonly List<CollectibleStar> Registry = new List<CollectibleStar>();
+    [Header("Config")]
+    [SerializeField] int value = 1;
+    [SerializeField] float spinSpeed = 90f;
+    [SerializeField] AudioClip sfxPickup;
+    [SerializeField] ParticleSystem vfxPickup;
+
+    bool _collected;
+
+    void Awake()
+    {
+        Registry.Add(this);
+    }
+
+    void OnDestroy()
+    {
+        Registry.Remove(this);
+    }
+
+    void OnEnable()
+    {
+        _collected = false;
+    }
+
+    void Update()
+    {
+        transform.Rotate(0f, spinSpeed * Time.deltaTime, 0f, Space.World);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (_collected) return;
+        if (!other.CompareTag("Player")) return;
+
+        _collected = true;
+
+        if (sfxPickup) AudioSource.PlayClipAtPoint(sfxPickup, transform.position);
+        if (vfxPickup) Instantiate(vfxPickup, transform.position, Quaternion.identity);
+
+        ScoreService.Add(value);
+
+        // Em vez de destruir, desativa para permitir reset posterior
+        gameObject.SetActive(false);
+    }
+
+    public static void ResetAll()
+    {
+        for (int i = 0; i < Registry.Count; i++)
+        {
+            var star = Registry[i];
+            if (star != null && !star.gameObject.activeSelf)
+                star.gameObject.SetActive(true);
+        }
+    }
+}
